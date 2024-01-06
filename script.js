410db793db0c6bf0fd98b7068eb4275d8833afed
christian.s.ramundo@gmail.com <christian.s.ramundo@gmail.com>
2024-01-06 17:52:23 -0300

    responsive 1.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const footer = document.getElementById('footer');
     const year = new Date().getFullYear();
-    footer.textContent = `Chocolocos ${year} ®`;
+    footer.textContent = ` ${year}`;
 });
